0af0fab33a4555213cb431d58080f5ce68021a69
Jhon Michael Turija <120067518+janing06@users.noreply.github.com>
2024-02-14 08:01:23 +0800

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["bilirandenguemonitoring.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 
